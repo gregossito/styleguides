f0f7388ccbd36d08a7b0db7e28bf1c1edd9212b6
Julie Arrigoni <julie.arrigoni@gmail.com>
2016-08-03 14:46:25 +0200

    Add block-map module and start implementing Mapbox

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -28,6 +28,7 @@ require('../modules/anchors-list/script');
 require('../modules/block-content-jecoute/script');
 require('../modules/block-content-tweet/script');
 require('../modules/block-content-newsletter/script');
+require('../modules/block-map/script');
 require('../modules/button-top/script');
 require('../modules/gallery-ugc/script');
 require('../modules/icon-switch/script');
---
+++ src/modules/block-map/script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// Load Mapbox dependency
+var mapboxgl = require('mapbox-gl');
+
+var Paris = window.Paris || {};
+
+Paris.blockMap = (function(){
+
+  var defaultOptions = {
+    style: 'mapbox://styles/mapbox/dark-v9',
+    zoom: 11,
+    minZoom: 10,
+    center: [2.349272, 48.856579],
+    maxBounds:
+      [
+       [2.021942, 48.731991], // SW coordinates
+       [2.698162, 48.985029]  // NE coordinates
+     ]
+  };
+
+  function blockMap(selector, userOptions){
+    var $el     = $(selector),
+        options = $.extend({}, defaultOptions, userOptions)
+
+    function init(){
+      initOptions();
+      initMap();
+
+      console.log(options);
+    }
+
+    function initOptions(){
+      $.each($el.data(), function(key, value){
+        options[key] = value;
+      });
+    }
+
+    function initMap(){
+      // TODO: Insert key into a config file?
+      mapboxgl.accessToken = 'pk.eyJ1IjoicGFyaXNudW1lcmlxdWUiLCJhIjoiY2loZG1vMnYyMDAzNnY0a3FvNG1nNG55biJ9.MP1qcHkEecFGqSTs9gg7cw';
+
+      var mapOptions = {
+        container: $el.attr('id')
+      };
+      $.extend(mapOptions, defaultOptions);
+      var map = new mapboxgl.Map(mapOptions);
+
+      // Add map
+      var nav = new mapboxgl.Navigation();
+      map.addControl(nav);
+    }
+
+    init();
+
+    return $el;
+  }
+
+  return function(selector, userOptions){
+    return $(selector).each(function(){
+      blockMap(this, userOptions);
+    });
+  };
+
+})();
+
+$(document).ready(function() {
+  console.log("Scrip for block-map ready");
+  // Mapbox expect only dom elements with an ID, do not pass a class name.
+  Paris.blockMap('#map');
+});
