b2882d50869394124436be142217afdd77d679ba
julienverkest <julienverkest@gmail.com>
2016-09-30 15:18:22 +0200

    modal video-home

--- src/modules/video-home/script.js
+++ src/modules/video-home/script.js
@@ -14,43 +14,19 @@ Paris.videoHome = (function(){
       $embed;
 
     function init(){
-
-      //$placeholder = $el.find('.video-home-placeholder');
-      // $embed = $el.find('.video-home-embed');
-
-      // if ($embed.length === 0) {return;}
-
-      // $placeholder.on('click', 'a[data-action="allow_cookies"]', onClickAllowCookies);
-
-      // renderVideo();
-      // PubSub.subscribe('cookies.updated', renderVideo);
       $el.find('a.video-home-item').on('click', function(event){
         event.preventDefault();
         var $embed = $(this).find('.video-home-embed').html();
+
         $('.video-home-modal .video-home-modal-body').empty().append($embed);
+
         $('.video-home-modal').show();
       });
 
       $('.video-home-close').on('click', function(){
+        $('.video-home-modal .video-home-modal-body').empty();
         $('.video-home-modal').hide();
       });
-    }
-
-    function onClickAllowCookies(e){
-      e.preventDefault();
-      PubSub.publish('cookies.allow');
-    }
-
-    function renderVideo(){
-      if ($embed === null || Cookies.get(Paris.config.cookies.cnil.name) !== Paris.config.cookies.cnil.value) {return;}
-      var embed = $embed.text();
-      $el.html(embed);
-      $embed.remove();
-      $embed = null;
-    }
-
-    function openModal(){
-
     }
 
     init();
