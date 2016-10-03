aeec7cd69f04ca9d1fc2980c9e6758fdf00cf0f7
julienverkest <julienverkest@gmail.com>
2016-10-03 17:39:52 +0200

    video home black

--- src/modules/video-home/script.js
+++ src/modules/video-home/script.js
@@ -17,8 +17,19 @@ Paris.videoHome = (function(){
       $el.find('a.video-home-item').on('click', function(event){
         event.preventDefault();
         var $embed = $(this).find('.video-home-embed').html();
+        var $placeholder = $('.video-home-placeholder').html();
 
-        $('.video-home-modal .video-home-modal-body').empty().append($embed);
+        if(Cookies.get(Paris.config.cookies.cnil.name) !== Paris.config.cookies.cnil.value) {
+          $('.video-home-modal .video-home-modal-body').empty().append($placeholder);
+        }
+        else {
+          $('.video-home-modal .video-home-modal-body').empty().append($embed);
+        }
+
+        $('.video-home-modal a[data-action="allow_cookies"]').on('click', onClickAllowCookies);
+        PubSub.subscribe('cookies.updated', function(){
+          $('.video-home-modal .video-home-modal-body').empty().append($embed);
+        });
 
         $('.video-home-modal').show();
       });
@@ -27,7 +38,15 @@ Paris.videoHome = (function(){
         $('.video-home-modal .video-home-modal-body').empty();
         $('.video-home-modal').hide();
       });
+
+      function onClickAllowCookies(e){
+        e.preventDefault();
+        PubSub.publish('cookies.allow');
+      }
+
     }
+
+
 
     init();
 
