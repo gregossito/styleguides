1bd16102b3d8ca1c7b92d698686a3dc90277e697
julienverkest <julienverkest@gmail.com>
2016-10-11 16:28:09 +0200

    add red-background

---
+++ build/assets/javascript/config.js
@@ -0,0 +1,45 @@
+var Paris = window.Paris || {};
+
+Paris.config = {
+  "algolia": {
+    // The Algolia application ID
+    "id": "QGS0I5WCQR",
+
+    // This API key should be created on Algolia, and have `search` permission on all the indexes below
+    "api_key": "219f93ef781ffa09cdb6803f702cf6f1",
+
+    // You can add any Algolia index here, to be able to use it in the quick-access module and search-results template
+    "indexes": {
+      "global": "recette_ParisFront",
+      "persons": "recette_Elus"
+    },
+
+    "url": {
+      "api_popular_searches": "http://r7.paris-fr-api.lestudio.mx/AlgoliaStats/getMostSearchedKeywords"
+    }
+  },
+  "cookies": {
+    "email": {
+      "name": "email"
+    },
+    "cnil": {
+      "name": "cookies",
+      "value": "accepted",
+      // `expires` can be a Number which will be interpreted as days from time of creation or a Date instance
+      "expires": 395 // 13 months
+    },
+    "parisconnect": {
+      "name": "pcuid"
+    },
+    "publicdata": {
+      "name": "publicdata"
+    },
+    "le3975": {
+      "host": "CTK_HOST-COOKIE",
+      "wssoId": "CTK_AGENTID-COOKIE"
+    }
+  },
+  "captcha": {
+    "key": "6Lf2DQoTAAAAAKmk3wEFCZuK3FqG00SlQM3o6Yvp"
+  }
+};
---
+++ build/assets/javascript/locales.js
@@ -0,0 +1,60 @@
+var Paris = window.Paris || {};
+Paris.i18n = Paris.i18n || {};
+
+Paris.i18n.locales = {
+  "fr": {
+    "share": {
+      "facebook": "Partager sur Facebook",
+      "twitter": "Partager sur Twitter",
+      "mail": "Partager par email"
+    },
+    "favorites": {
+      "add": "ajouter aux favoris",
+      "remove": "enlever des favoris"
+    },
+    "search_results": {
+      "search": "Recherche",
+      "title": {
+        "one": "Un résultat",
+        "plural": "{0} résultats"
+      },
+      "page": "Page {0}",
+      "no_result": "Désolé, votre recherche ne correspond à aucun de nos contenus.",
+      "more": "Afficher plus de résultats",
+      "facets": {
+        "onglet": "Catégories",
+        "groupe_politique": "Groupe politique",
+        "secteur": "Arrondissement"
+      }
+    },
+    "list_persons": {
+      "no_result": "Personne ne correspond à votre recherche."
+    },
+    "person_block": {
+      "button_view": "Voir la fiche"
+    },
+    "pagination": {
+      "label": "Pagination des résultats",
+      "prev": "Page précédente",
+      "next": "Page suivante",
+      "link_title": "Aller à la page ${page}"
+    },
+    "jecoute": {
+      "error": {
+        "policy": "Veuillez accepter la charte.",
+        "file_size": "Le poids total des pièces jointes est trop élevé."
+      }
+    },
+    "alerts": {
+      "subscribe": "Je m’abonne à l’alerte pour recevoir toutes les informations sur ce sujet",
+      "unsubscribe": "Je me désabonne de l’alerte sur ce sujet"
+    },
+    "postit": {
+      "default_title": "Informations complémentaires"
+    },
+    "active": "actif",
+    "inactive": "inactif",
+    "new_window": "nouvelle fenêtre",
+    "close_nav": "fermer le menu \"{0}\""
+  }
+};
