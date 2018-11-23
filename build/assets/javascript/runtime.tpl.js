21ffc75ebab469607ac8792e343f047b25ebf9cf
julienverkest <julienverkest@gmail.com>
2018-11-23 16:31:42 +0100

    13 photos

--- build/assets/javascript/runtime.tpl.js
+++ build/assets/javascript/runtime.tpl.js
@@ -3356,6 +3356,26 @@ templatizer["block-content"]["block-content"] = function tmpl_block_content_bloc
     return buf.join("");
 };
 
+// block-map.jade compiled template
+templatizer["block-map"] = function tmpl_block_map(locals) {
+    var buf = [];
+    var jade_mixins = {};
+    var jade_interp;
+    var locals_for_with = locals || {};
+    (function(JSON) {}).call(this, "JSON" in locals_for_with ? locals_for_with.JSON : typeof JSON !== "undefined" ? JSON : undefined);
+    return buf.join("");
+};
+
+// block-map.jade:block-map compiled template
+templatizer["block-map"]["block-map"] = function tmpl_block_map_block_map(data) {
+    var block = this && this.block, attributes = this && this.attributes || {}, buf = [], jade_interp;
+    data = JSON.parse(JSON.stringify(data));
+    data.modifiers = data.modifiers || [];
+    data.modifiers.push("block-map");
+    buf.push('<div id="map"></div><div class="map-caption"><div class="map-caption-icon is-open"></div>' + jade.escape(null == (jade_interp = "Équipement ouvert") ? "" : jade_interp) + '<div class="map-caption-icon is-closed"></div>' + jade.escape(null == (jade_interp = "Équipement fermé") ? "" : jade_interp) + '<div class="map-caption-icon is-unknown"></div>' + jade.escape(null == (jade_interp = "Horaires non renseignés") ? "" : jade_interp) + "</div>");
+    return buf.join("");
+};
+
 // block-search-field.jade compiled template
 templatizer["block-search-field"] = function tmpl_block_search_field(locals) {
     var buf = [];
@@ -3471,26 +3491,6 @@ templatizer["block-search-field"]["block-search-field"] = function tmpl_block_se
             buf.push(templatizer["block-search-field"]["search-field"](data.search_field));
         }
     }, data));
-    return buf.join("");
-};
-
-// block-map.jade compiled template
-templatizer["block-map"] = function tmpl_block_map(locals) {
-    var buf = [];
-    var jade_mixins = {};
-    var jade_interp;
-    var locals_for_with = locals || {};
-    (function(JSON) {}).call(this, "JSON" in locals_for_with ? locals_for_with.JSON : typeof JSON !== "undefined" ? JSON : undefined);
-    return buf.join("");
-};
-
-// block-map.jade:block-map compiled template
-templatizer["block-map"]["block-map"] = function tmpl_block_map_block_map(data) {
-    var block = this && this.block, attributes = this && this.attributes || {}, buf = [], jade_interp;
-    data = JSON.parse(JSON.stringify(data));
-    data.modifiers = data.modifiers || [];
-    data.modifiers.push("block-map");
-    buf.push('<div id="map"></div><div class="map-caption"><div class="map-caption-icon is-open"></div>' + jade.escape(null == (jade_interp = "Équipement ouvert") ? "" : jade_interp) + '<div class="map-caption-icon is-closed"></div>' + jade.escape(null == (jade_interp = "Équipement fermé") ? "" : jade_interp) + '<div class="map-caption-icon is-unknown"></div>' + jade.escape(null == (jade_interp = "Horaires non renseignés") ? "" : jade_interp) + "</div>");
     return buf.join("");
 };
 
@@ -4542,6 +4542,77 @@ templatizer["jumbotron-slider"]["jumbotron-slider"] = function tmpl_jumbotron_sl
         buf.push('<i class="icon-arrow-small-right"></i>');
     }
     buf.push("</span></p></div></" + tag + ">");
+    return buf.join("");
+};
+
+// jumbotron-treize.jade compiled template
+templatizer["jumbotron-treize"] = function tmpl_jumbotron_treize(locals) {
+    var buf = [];
+    var jade_mixins = {};
+    var jade_interp;
+    var locals_for_with = locals || {};
+    (function(JSON) {}).call(this, "JSON" in locals_for_with ? locals_for_with.JSON : typeof JSON !== "undefined" ? JSON : undefined);
+    return buf.join("");
+};
+
+// jumbotron-treize.jade:button compiled template
+templatizer["jumbotron-treize"]["button"] = function tmpl_jumbotron_treize_button(data) {
+    var block = this && this.block, attributes = this && this.attributes || {}, buf = [], jade_interp;
+    var data = JSON.parse(JSON.stringify(data || {}));
+    var attributes = data.attributes || {};
+    var modifiers = data.modifiers || [];
+    if (data.icon) {
+        modifiers.push("icon");
+    }
+    if (data.href) {
+        if (attributes.disabled) {
+            delete attributes.disabled;
+            modifiers.push("disabled");
+        }
+        buf.push("<a" + jade.attrs(jade.merge([ {
+            href: jade.escape(data.href),
+            onclick: jade.escape(data.onclick),
+            title: jade.escape(data.title),
+            target: jade.escape(data.target),
+            "class": (jade_interp = [ null, true ], jade.joinClasses([ "button", modifiers ].map(jade.joinClasses).map(function(cls, i) {
+                return jade_interp[i] ? jade.escape(cls) : cls;
+            })))
+        }, attributes ]), false) + ">");
+        if (modifiers && modifiers.indexOf("marker") !== -1) {
+            buf.push('<i aria-hidden="true" class="icon icon-marker"></i>');
+        } else if (data.icon) {
+            buf.push('<i aria-hidden="true"' + jade.cls([ "icon", "icon-" + data.icon + "" ], [ null, true ]) + "></i>");
+        }
+        buf.push('<span class="button-text">' + jade.escape(null == (jade_interp = data.text) ? "" : jade_interp) + "</span></a>");
+    } else {
+        buf.push("<button" + jade.attrs(jade.merge([ {
+            title: jade.escape(data.title),
+            "class": (jade_interp = [ null, true ], jade.joinClasses([ "button", modifiers ].map(jade.joinClasses).map(function(cls, i) {
+                return jade_interp[i] ? jade.escape(cls) : cls;
+            })))
+        }, attributes ]), false) + ">");
+        if (modifiers && modifiers.indexOf("marker") !== -1) {
+            buf.push('<i aria-hidden="true" class="icon icon-marker"></i>');
+        } else if (data.icon) {
+            buf.push('<i aria-hidden="true"' + jade.cls([ "icon", "icon-" + data.icon + "" ], [ null, true ]) + "></i>");
+        }
+        buf.push('<span class="button-text">' + jade.escape(null == (jade_interp = data.text) ? "" : jade_interp) + "</span></button>");
+    }
+    return buf.join("");
+};
+
+
+// jumbotron-treize.jade:jumbotron-treize compiled template
+templatizer["jumbotron-treize"]["jumbotron-treize"] = function tmpl_jumbotron_treize_jumbotron_treize(data) {
+    var block = this && this.block, attributes = this && this.attributes || {}, buf = [], jade_interp;
+    buf.push("<div" + jade.attr("style", "background-image: url(" + data.image + ");", true, false) + jade.attr("id", data.id, true, false) + jade.cls([ "jumbotron-treize", data.modifiers ], [ null, true ]) + "><div" + jade.attr("style", "background-image: url(" + data.masque + ");", true, false) + ' class="jumbotron-treize-wrapper"><div class="jumbotron-treize-content">');
+    var tag = data.tag || "h2";
+    buf.push("<" + tag + ' class="jumbotron-treize-title">' + jade.escape(null == (jade_interp = data.title) ? "" : jade_interp) + "</" + tag + '><div class="jumbotron-treize-text">' + jade.escape(null == (jade_interp = data.text) ? "" : jade_interp) + '</div><div class="treize-credit">' + jade.escape(null == (jade_interp = data.credit) ? "" : jade_interp) + "</div>");
+    if (data.button) {
+        var button = JSON.parse(JSON.stringify(data.button));
+        buf.push(templatizer["jumbotron-treize"]["button"](button));
+    }
+    buf.push("</div></div></div>");
     return buf.join("");
 };
 
@@ -7814,6 +7885,73 @@ templatizer["tags"]["tags"] = function tmpl_tags_tags(data) {
                 if (index < data.length - 1) {
                     buf.push('<span class="tags-separator">—</span>');
                 }
+                buf.push("</li>");
+            }
+        }
+    }).call(this);
+    buf.push("</ul>");
+    return buf.join("");
+};
+
+// treize-card.jade compiled template
+templatizer["treize-card"] = function tmpl_treize_card(locals) {
+    var buf = [];
+    var jade_mixins = {};
+    var jade_interp;
+    return buf.join("");
+};
+
+// treize-card.jade:treize-card compiled template
+templatizer["treize-card"]["treize-card"] = function tmpl_treize_card_treize_card(data) {
+    var block = this && this.block, attributes = this && this.attributes || {}, buf = [], jade_interp;
+    buf.push("<a" + jade.attr("href", data.href, true, false) + jade.attr("onclick", data.onclick, true, false) + jade.attr("target", data.target, true, false) + jade.cls([ "treize-card", data.modifiers ], [ null, true ]) + '><div class="treize-card-wrapper"><div' + jade.attr("style", data.image && "background-image: url(" + data.image + ");", true, false) + ' class="treize-card-image"></div><div class="treize-card-content"><div class="treize-card-date"><span>' + jade.escape(null == (jade_interp = data.date) ? "" : jade_interp) + '</span></div><div class="treize-card-title">');
+    if (data.target === "_blank") {
+        buf.push('<span class="icon-link-external"></span>');
+    }
+    buf.push(jade.escape(null == (jade_interp = data.title) ? "" : jade_interp) + '</div><div class="treize-card-credit"><span>' + jade.escape(null == (jade_interp = data.credit) ? "" : jade_interp) + "</span></div></div></div></a>");
+    return buf.join("");
+};
+
+// treize-list.jade compiled template
+templatizer["treize-list"] = function tmpl_treize_list(locals) {
+    var buf = [];
+    var jade_mixins = {};
+    var jade_interp;
+    return buf.join("");
+};
+
+// treize-list.jade:treize-card compiled template
+templatizer["treize-list"]["treize-card"] = function tmpl_treize_list_treize_card(data) {
+    var block = this && this.block, attributes = this && this.attributes || {}, buf = [], jade_interp;
+    buf.push("<a" + jade.attr("href", data.href, true, false) + jade.attr("onclick", data.onclick, true, false) + jade.attr("target", data.target, true, false) + jade.cls([ "treize-card", data.modifiers ], [ null, true ]) + '><div class="treize-card-wrapper"><div' + jade.attr("style", data.image && "background-image: url(" + data.image + ");", true, false) + ' class="treize-card-image"></div><div class="treize-card-content"><div class="treize-card-date"><span>' + jade.escape(null == (jade_interp = data.date) ? "" : jade_interp) + '</span></div><div class="treize-card-title">');
+    if (data.target === "_blank") {
+        buf.push('<span class="icon-link-external"></span>');
+    }
+    buf.push(jade.escape(null == (jade_interp = data.title) ? "" : jade_interp) + '</div><div class="treize-card-credit"><span>' + jade.escape(null == (jade_interp = data.credit) ? "" : jade_interp) + "</span></div></div></div></a>");
+    return buf.join("");
+};
+
+
+// treize-list.jade:treize-list compiled template
+templatizer["treize-list"]["treize-list"] = function tmpl_treize_list_treize_list(data) {
+    var block = this && this.block, attributes = this && this.attributes || {}, buf = [], jade_interp;
+    buf.push("<ul" + jade.cls([ "treize-list", data.modifiers ], [ null, true ]) + ">");
+    (function() {
+        var $$obj = data.items;
+        if ("number" == typeof $$obj.length) {
+            for (var $index = 0, $$l = $$obj.length; $index < $$l; $index++) {
+                var item = $$obj[$index];
+                buf.push('<li class="treize-list-card-item">');
+                buf.push(templatizer["treize-list"]["treize-card"](item));
+                buf.push("</li>");
+            }
+        } else {
+            var $$l = 0;
+            for (var $index in $$obj) {
+                $$l++;
+                var item = $$obj[$index];
+                buf.push('<li class="treize-list-card-item">');
+                buf.push(templatizer["treize-list"]["treize-card"](item));
                 buf.push("</li>");
             }
         }
---
+++ src/modules/treize-list/script.js
@@ -0,0 +1,167 @@
+'use strict';
+
+var Paris = window.Paris || {};
+
+Paris.newsList = (function(){
+
+  var defaultOptions = {
+  };
+
+  function newsList(selector, userOptions){
+    var $el     = $(selector),
+        options = $.extend({}, defaultOptions, userOptions),
+        $next, $prev, $listMonths, $date, $arrondissements, $button;
+
+    function init(){
+      initOptions();
+
+      $listMonths = ['Jan.', 'Févr.', 'Mars', 'Avr.', 'Mai', 'Juin', 'Juill.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'];
+      $date =  $('.treize-list-agenda-nav-month'); 
+      $arrondissements = $('.treize-list-agenda-arrondissement');
+      var current_month = new Date().getMonth();
+
+      // List of months, start with current month
+      for (var i = current_month; i < 12; i++) {
+        var m = i+1;
+        $date.append('<span data-month="' + m + '" data-year="' + new Date().getFullYear() + '">' + $listMonths[i]+ '</span>');
+      }
+      for (var i = 0; i < current_month; i++) {
+        var m = i+1;
+        var y = parseInt(new Date().getFullYear())+1;
+        $date.append('<span data-month="' + m + '" data-year="' + y + '">' + $listMonths[i]+ '</span>');
+      }
+
+      // Filters action
+      $('.treize-list-agenda-filters select').change(function () {
+        $( ".treize-list-agenda-filters select option:selected" ).each(function() {
+          if($(this).val() == 'arr'){
+            $date.hide();
+            $arrondissements.show();
+          }
+          else {
+            $date.show();
+            $arrondissements.hide();
+          }
+        });
+        displayAllNews();
+      })
+      .change();
+
+      sortNewsAndFilter();
+      $date.children('span').on('click', monthFilters);
+      $arrondissements.children('span').on('click', arrFilters);
+      initHaveActus();
+    }
+
+    function initOptions() {
+      $.each($el.data(), function(key, value){
+        options[key] = value;
+      });
+    }
+
+    // En surbrillance les arrondissements et les mois qui ont une actu 
+    function initHaveActus() {
+      $('.agenda .treize-list-card-item').each(function(){
+        var date = $(this).find('.treize-card-date span').text();
+        date = date.split('/');
+        var arr = $(this).find('.treize-card-arrondissement span').text();
+        $('.treize-list-agenda-arrondissement-change').eq(arr-1).addClass('have-actus');
+        $(".treize-list-agenda-nav-month span[data-month="+parseInt(date[1])+"][data-year="+parseInt(date[2])+"]").addClass('have-actus');
+      });
+    }
+
+    // Click on arrondissement
+    function arrFilters(arr) {
+      var that = $(this);
+      if(that.hasClass('have-actus')) {
+        if (that.hasClass('active')) {
+          displayAllNews();
+        }
+        else {
+          $('.treize-list-agenda-arrondissement-change').removeClass('active');
+          that.addClass('active');
+          var a = that.text();
+          $('.agenda').hide();
+          $('.agenda-clone').empty();
+          $('.agenda .treize-list-card-item').each(function(){
+            var arr = $(this).find('.treize-card-arrondissement span').text();
+            if(parseInt(a) == parseInt(arr)) {
+              $('.agenda-clone').append('<li class="treize-list-card-item">' + $(this).html() + '</li>');
+            }
+          });
+        }
+      }
+    } 
+
+    // Click on month
+    function monthFilters(month) {
+      var that = $(this);
+      if(that.hasClass('have-actus')) {
+        if (that.hasClass('active')) {
+          displayAllNews();
+        }
+        else {
+          $('.treize-list-agenda-nav-month span').removeClass('active');
+          that.addClass('active');
+          var m = that.data('month');
+          var y = that.data('year');
+          $('.agenda').hide();
+          $('.agenda-clone').empty();
+          $('.agenda .treize-list-card-item').each(function(){
+            var date = $(this).find('.treize-card-date span').text();
+            date = date.split('/');
+            if(m == parseInt(date[1]) && y == parseInt(date[2])) {
+              $('.agenda-clone').append('<li class="treize-list-card-item">' + $(this).html() + '</li>');
+            }
+          });
+        }
+      }
+    } 
+
+    function displayAllNews() {
+      $('.agenda-clone').empty();
+      $('.agenda').fadeIn();
+      $('.treize-list-agenda-nav-month span').removeClass('active');
+    }
+
+    function sortNewsAndFilter() {
+      var m = new Date().getMonth()+1;
+      if(m.length<2) { m = "0"+m; }
+      var y = new Date().getFullYear();
+      var d = new Date().getDate();
+      var today =String(y)+String(m)+String(d);
+      today = parseInt(today)
+      $('.agenda .treize-list-card-item').each(function(){
+        var date = $(this).find('.treize-card-date span').text();
+        date = date.split('/');
+        $(this).attr('data-date', date[2] + date[1] + date[0]);
+        var currDate = String(date[2])+String(date[1])+String(date[0]);
+        currDate = parseInt(currDate);
+        // remove old news
+        if(today>currDate) {
+          $(this).remove();
+        }
+      })
+      $('.agenda').each(function(){
+        $(this).html($(this).children('li').sort(function(a, b){
+          return ($(b).data('date')) < ($(a).data('date')) ? 1 : -1;
+        }));
+      });
+    }
+
+    init();
+
+    return $el;
+  }
+
+  return function(selector, userOptions){
+    return $(selector).each(function(){
+      newsList(this, userOptions);
+    });
+  };
+
+})();
+
+$(document).ready(function(){
+  Paris.newsList('.treize-list-agenda');
+});
